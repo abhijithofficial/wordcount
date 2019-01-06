3fb77c1e38bc054e145ddf1797a15fe217061a0e
Abhijith <alwayzabhijith@gmail.com>
2019-01-06 12:35:46 +0530

    Added count in h1,print text in para

--- wordcount/new.py
+++ wordcount/new.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 def count(request):
     fulltext=request.GET['name']
     fulltext=fulltext.split()
-    return render(request,'count.html',{'full':fulltext})
+    return render(request,'count.html',{'full':len(fulltext),'give':fulltext})
