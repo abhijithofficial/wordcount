b5bfe138891c5457077dd1a62a5ee97c0e9c8d1c
Abhijith <alwayzabhijith@gmail.com>
2019-01-06 12:53:30 +0530

    sort words by count added

--- wordcount/new.py
+++ wordcount/new.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+import operator
 def count(request):
     fulltext=request.GET['name']
     fulltext=fulltext.split()
@@ -9,4 +10,5 @@ def count(request):
             worddict[word]+=1
         else:
             worddict[word]=1
-    return render(request,'count.html',{'full':len(fulltext),'give':fulltext,'worditem':worddict.items})
+    sorterlar=sorted(worddict.items(),key=operator.itemgetter(1),reverse=True)
+    return render(request,'count.html',{'full':len(fulltext),'give':fulltext,'worditem':worddict.items(),'large':sorterlar})
