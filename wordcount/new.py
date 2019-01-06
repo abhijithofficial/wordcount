275f1a151b5b43c916cad5cd1fc4e91534a14c2e
Abhijith <alwayzabhijith@gmail.com>
2019-01-06 12:40:38 +0530

    word dictionary added

--- wordcount/new.py
+++ wordcount/new.py
@@ -3,4 +3,10 @@ from django.shortcuts import render
 def count(request):
     fulltext=request.GET['name']
     fulltext=fulltext.split()
-    return render(request,'count.html',{'full':len(fulltext),'give':fulltext})
+    worddict={}
+    for word in fulltext:
+        if word in worddict:
+            worddict['word']+=1
+        else
+            worddict['word']=1
+    return render(request,'count.html',{'full':len(fulltext),'give':fulltext,'worditem':worddict.items})
